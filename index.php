5800002f8d1d011ff9540130f86248573e482470
Ivan <onel1n.van@yandex.ru>
2024-11-01 15:15:20 +0300

    signup form complete

--- index.php
+++ index.php
@@ -1,11 +1,46 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
     <title>ChillChat</title>
 </head>
 <body>
-    
+    <div class="wrapper">
+        <section class="form signup">
+            <header>ChillChat</header>
+            <form action="#">
+                <div class="error-txt">Здесь выводится сообщение об ошибке</div>
+                <div class="name-details">
+                    <div class="field input">
+                        <label>First Name</label>
+                        <input type="text" placeholder="First Name">
+                    </div>
+                    <div class="field input">
+                        <label>Last Name</label>
+                        <input type="text" placeholder="Last Name">
+                    </div>
+                </div>
+                <div class="field input">
+                    <label>Email Address</label>
+                    <input type="text" placeholder="Enter your email">
+                </div>
+                <div class="field input">
+                    <label>Password</label>
+                    <input type="text" placeholder="Enter Password">
+                </div>
+                <div class="field image">
+                    <label>Select Image</label>
+                    <input type="file">
+                </div>
+                <div class="field buttom">
+                    <input type="submit" value="Continue to Chat">
+                </div>
+            </form>
+            <div class="link">Already signed up? <a href="#">Login now</a></div>
+        </section>
+    </div>
 </body>
 </html>
