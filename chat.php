f4271efdf88f8e272bb343308c871db9e12a6b4b
Ivan <onel1n.van@yandex.ru>
2024-11-03 23:45:59 +0300

    chat page header

---
+++ chat.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>ChillChat</title>
+</head>
+<body>
+    <div class="wrapper">
+        <section class="chat-area">
+            <header>
+                <a href="#" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+                <img src="./img/sova.jpg" alt="">
+                <div class="details">
+                    <span>Sova GitHub</span>
+                    <p>Active</p>
+                </div>
+            </header>
+        </section>
+    </div>
+</body>
+</html>
