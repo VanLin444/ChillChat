e6ddf363b2bd9c8043db9ba33fa6179bd2cece1b
SOVA <79813883+VanLin444@users.noreply.github.com>
2024-11-06 21:45:43 +0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,1 +1,12 @@
+<?php
+    $dbHost = 'localhost';
+    $dbUsername = 'root';
+    $dbPassword = 'root';
+    $dbName = 'chill-chat';
+    $dbPort = '3306';
 
+    $conn = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName, $dbPort);
+
+    if (!$conn) die("Ошибка подключения к БД - " . mysqli_connect_error());
+
+?>
