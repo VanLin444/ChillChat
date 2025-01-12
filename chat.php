8e1ac597f1eea0e7ec2919b75bc782169d940203
Ivan <onel1n.van@yandex.ru>
2025-01-12 23:46:54 +0300

    work with chat

--- chat.php
+++ chat.php
@@ -1,3 +1,10 @@
+<?php 
+  session_start();
+  include_once "php/config.php";
+  if(!isset($_SESSION['unique_id'])){
+    header("location: login.php");
+  }
+?>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -11,11 +18,19 @@
     <div class="wrapper">
         <section class="chat-area">
             <header>
-                <a href="#" class="back-icon"><i class="fas fa-arrow-left"></i></a>
-                <img src="./img/sova.jpg" alt="">
+                <?php
+                    include_once "/php/config.php";
+                    $user_id = mysqli_real_escape_string($conn, $_GET['user_id']);
+                    $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$user_id}");
+                    if(mysqli_num_rows($sql) > 0){
+                        $row = mysqli_fetch_assoc($sql);
+                    }
+                ?>    
+                <a href="users.php" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+                <img src="img/<?php echo $row['image'] ?>" alt="avatar">
                 <div class="details">
-                    <span>Sova GitHub</span>
-                    <p>Active</p>
+                    <span><?php echo $row['fname'] . " " . $row['lname']?></span>
+                    <p><?php echo $row['status']?></p>
                 </div>
             </header>
             <div class="chat-box">
@@ -65,10 +80,14 @@
                 </div>
             </div>
             <form action="#" class="typing-area">
-                <input type="text" placeholder="Write a message...">
+                <input tepe="text" name="outgoing_id" value="<?php echo $_SESSION['unique_id']?>">
+                <input tepe="text" name="incoming_id" value="<?php echo $user_id?>">
+                <input type="text" name="message" class="input-field" placeholder="Write a message...">
                 <button><i class="fab fa-telegram-plane"></i></button>
             </form>
         </section>
     </div>
+
+    <script src="js/chat.js"></script>
 </body>
 </html>
--- php/data.php
+++ php/data.php
@@ -1,6 +1,6 @@
 <?php
     while($row = mysqli_fetch_assoc($sql)){
-        $output .= '<a href="#">
+        $output .= '<a href="chat.php?user_id='.$row['unique_id'].'">
                     <div class="content">
                     <img src="img/'. $row['image'] .'" alt="avatar">
                     <div class="details">
