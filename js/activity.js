44571567bbd97b49fa7ba9e09f88dd58c4b07863
Ivan <onel1n.van@yandex.ru>
2025-01-23 22:52:14 +0300

    user activity functionality completed

--- js/activity.js
+++ js/activity.js
@@ -3,10 +3,9 @@ function checkActivity() {
     clearTimeout(timeout);
     timeout = setTimeout(function () { 
         let xhr = new XMLHttpRequest();
-        xhr.open("GET", "php/logout.php");
-        let data = "logout_id=" + userId;
-        xhr.send(data);
-     }, 5000);
+        xhr.open("GET", "php/logout.php?logout_id=" + userId);
+        xhr.send();
+     }, 300000);
 }
 document.addEventListener('keydown', checkActivity);
 document.addEventListener('mousedown', checkActivity);
--- php/logout.php
+++ php/logout.php
@@ -3,7 +3,6 @@
     if(isset($_SESSION['unique_id'])){
         include_once "config.php";
         $logout_id = mysqli_real_escape_string($conn, $_GET['logout_id']);
-        echo $_GET['logout_id'];
         if(isset($logout_id)){
             $status = "Offline now";
             $sql = mysqli_query($conn, "UPDATE users SET status = '{$status}' WHERE unique_id = {$logout_id}");
